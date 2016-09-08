8d1ec343efb5b436251e8e6a12db4c2fc9a7ec0a
Nicolas Berbiche <nic.berbiche@outlook.com>
2016-09-07 21:48:44 -0400

    Test of a singleton implementation in Java

--- Java/singleton/principal.java
+++ Java/singleton/principal.java
@@ -1,8 +1,22 @@
 
-class Principal {
+public class Singleton {
 
-    public static void Main(String[] args) {
+    private static Singleton singleton = new Singleton();
+    private int x;
 
+    /**
+     * Private constructor
+     */
+    private Singleton() {
+        this.x = 0;
+    }
+
+    /**
+     * Returns the only instance of the singleton
+     * @return the only instance of the singleton
+     */
+    public static Singleton getInstance() {
+        return singleton;
     }
 
 }
